0a0279a65ac57e7b9b70ea3c5b3d6e1d78195577
shaunpwl <32692177+shaunpwl@users.noreply.github.com>
2017-10-25 17:26:39 +0800

    Add files via upload

---
+++ AddtoCartShoe2.php
@@ -0,0 +1,49 @@
+<?php //AddtoCartShoe2.php
+session_start();
+if (!isset($_SESSION['cart'])){
+	$_SESSION['cart'] = array();
+}
+if (isset($_GET['buy'])) {
+	$_SESSION['cart'][] = $_GET['buy'];
+	header('location: ' . $_SERVER['PHP_SELF']. '?' . SID);
+	exit();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Product catalog</title>
+</head>
+<body>
+<p>Your shopping cart contains <?php
+	echo count($_SESSION['cart']); ?> items.</p>
+<p><a href="CheckoutPage.php">View your cart</a></p>
+
+<?php
+$items = array(
+	'Shoe 2',
+	);
+$prices = array(50);
+?>
+<table border="1">
+	<thead>
+	<tr>
+		<th>Item Description</th>
+		<th>Price</th>
+	</tr>
+	</thead>
+	<tbody>
+<?php
+for ($i=0; $i<count($items); $i++){
+	echo "<tr>";
+	echo "<td>" .$items[$i]. "</td>";
+	echo "<td>$" .number_format($prices[$i], 2). "</td>";
+	echo "<td><a href='" .$_SERVER['PHP_SELF']. '?buy=' .$i. "'>Buy</a></td>";
+	echo "</tr>";
+}
+?>
+	</tbody>
+</table>
+<p>All prices are in imaginary dollars.</p>
+</body>
+</html>
